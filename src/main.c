6c73ab5ff8ea45eacbfb5b53ce0aaed1f227d12a
Sujong Ham <rnfhrn960@gmail.com>
2023-06-14 22:44:03 +0900

    update

--- src/main.c
+++ src/main.c
@@ -31,13 +31,28 @@ typedef struct _Player {
 	int state;	// 상태: Hit / Stand 
 } Player;
 
+unsigned char pattern[3][8] = {
+	{0x00, 0x24, 0x24, 0x24, 0x00, 0x42, 0x7E, 0x00},	// 웃는거 
+	{0x00, 0x24, 0x24, 0x24, 0x00, 0x7E, 0x42, 0x00},	// 우는거 
+	{0x00, 0x24, 0x24, 0x24, 0x00, 0x54, 0x2A, 0x00}	// 비긴거 
+};
+
+int fnd_dev;
+int lcd_dev;
+int tact_dev;
+int dot_dev;
+char player_arr[32] = "  Player Cards  ";
+char dealer_arr[32] = "  Dealer Cards  ";
+
 /* 함수 원형 */ 
 void error(char* message);
 void print_pattern(int number);
  
 /* Deck */
 void init(Deck* D);												// 덱 생성 
-void print_deck(Deck *D);										// 덱 출력 
+void print_deck(Deck *D);										// 덱 출력
+void get_player_deck(Deck *D);
+void get_dealer_deck(Deck *D);
 int is_empty(Deck *D);											// 덱이 비었는지 확인
 int is_full(Deck *D);											// 덱이 찼는지 확인
 int has_pair(Deck *D);											// 덱에 페어가 있는지 확인 
@@ -49,36 +64,17 @@ void draw(Deck *D, Deck *S, Player *player);					// 카드 드로우
 
 /* FND */ 
 unsigned char asc_to_fnd(int n); 
-void write_fnd(int dev, int dealer_score, int player_score);	// FND에 딜러 및 플레이어의 점수 표기
+void set_fnd(int dealer_score, int player_score);				// FND에 딜러 및 플레이어의 점수 표기
+void set_lcd(char P[]);
+int get_tact();
+void set_dot(int index);
 //unsigned char get_tact(int tmo);
 
 // static tactswDev[] = "/dev/tactsw";	// tactsw
 // static int tactswFd = (-1);
 
 int main(int argc, char *argv[]) {
-	
-	int fnd_dev = open("/dev/fnd", O_RDWR);
-	if (fnd_dev < 0) {
-		fprintf(stderr, "can't open FND (%d)", fnd_dev);
-		exit(2);
-	}
-	int lcd_dev = open("/dev/clcd", O_RDWR);
-	if (lcd_dev < 0) {
-		fprintf(stderr, "can't open LCD (%d)", lcd_dev);
-		exit(2);
-	}
-	int tact_dev = open("/dev/tactsw", O_RDWR);
-	if (tact_dev < 0) {
-		fprintf(stderr, "can't open TACT (%d)", tact_dev);
-		exit(2);
-	}
-	
-//	unsigned char buf[MAX_CLCD_CHAR];
-//	memset(buf, 0, sizeof(buf));
-//	char message[MAX_CLCD_CHAR] = "Welcome !!\0";
-//	strcpy(message, s)
-	printf("%s\n", message);
-	write(lcd_dev, "Hello World", MAX_CLCD_CHAR);
+
 	
 	while(1) {
 		/* 게임 루프 변후 */ 
@@ -87,7 +83,8 @@ int main(int argc, char *argv[]) {
 		int burst = 0;		
 		/* 승자 결정 변수			: 1이면 플레이어 승리, -1이면 딜러 승리, 0이면 무승부 */ 
 		int score = 0;	
-		
+		set_lcd("                   Game Start   ");
+		usleep(3 * 500000);
 		// 상태를 초기화한다. 
 		Deck player_stack; init(&player_stack);		// 플레이어 카드 
 		Deck dealer_stack; init(&dealer_stack);		// 딜러 카드 
@@ -113,7 +110,11 @@ int main(int argc, char *argv[]) {
 
 		// 게임 루프 
 		while(1) {
-			write_fnd(fnd_dev, dealer.score, player.score);										// ######
+			char new_player_arr[32] = "  Player Cards  ";
+			char new_dealer_arr[32] = "  Dealer Cards  ";
+			strcpy(player_arr, new_player_arr);
+			strcpy(dealer_arr, new_dealer_arr);
+			// 게임 종료 조건
 			if ((player.score > 21) && !has_pair(&player_stack)) {
 				burst++;
 				score--;
@@ -122,59 +123,117 @@ int main(int argc, char *argv[]) {
 				burst++;
 				score++;
 			}
-			if (burst) {
-				switch(score) {
-					case 1: printf("\n\nPlayer Win!"); break;
-					case 0: printf("\n\n무승부!!"); break;	// 여기서 점수 산정 함수를 추가해야 함. 
-					case -1: printf("\n\nDealer Win!"); break;
-					default: error("burst error");
-				}
-				break;
-			}
+			if (burst) break;
 			if (player.state == STAND && dealer.state == STAND) {
-				int score_difference = abs(player.score - 20.9) - abs(dealer.score -20.9);
+				double score_difference = abs(player.score - 20.9) - abs(dealer.score -20.9);
 				if (score_difference < 0) {
-					printf("\n\nPlayer Win!");
+					score = 1;
 				} else if (score_difference > 0) {
-					printf("\n\nDealer Win!");
+					score = -1;
 				} else {
-					printf("\n\n무승부!!");
+					score = 0;
 				}
 				break;
 			}
-			if (player.state == HIT) {
+			
+			// 게임 진행 
+			int tt;
+			set_lcd("                Dealer----Player");
+			set_fnd(dealer.score, player.score);
+			get_player_deck(&player_stack);
+			set_lcd(player_arr);
+			usleep(3 * 500000);
+			while(player.state == HIT) {
 				printf("\n====================\nPlayer Turn, ");
-				print_deck(&player_stack); printf("  => %d점\n", player.score);
-				printf("Hit? (Y: 1, N: 0) "); scanf("%d", &player.state);
+				print_deck(&player_stack);
+				printf("  => %d점\n", player.score);
+				set_lcd("  Player Turn!  Hit ? or Stand ?");
+				tt = get_tact();		
+				if (tt == 5) {
+					set_lcd("                Dealer    Player");
+					set_fnd(dealer.score, player.score);
+					continue;
+				}
+				while(1) {
+					if (tt == 4) {
+						player.state = HIT;	
+					} else if (tt == 6) {
+						player.state = STAND;
+					} else {
+						continue;	
+					}
+					break;
+				}
+
 				if (player.state == HIT) {
 					peek(&deck);
 					draw(&deck, &player_stack, &player);
 					printf(" => %d점", player.score);
-					write_fnd(fnd_dev, dealer.score, player.score);								// ######	
+					set_lcd("                Dealer    Player");
+					set_fnd(dealer.score, player.score);
 				}
+				break;
 			}
-			if (dealer.state == HIT) {
+			get_dealer_deck(&dealer_stack);
+			set_lcd(dealer_arr);
+			usleep(3 * 500000);
+			while(dealer.state == HIT) {
 				printf("\n====================\nDealer Turn, ");
-				print_deck(&dealer_stack); printf("  => %d점\n", dealer.score);
+				print_deck(&dealer_stack);
+				printf("  => %d점\n", dealer.score);
+				set_lcd("                  Dealer Turn!  ");
+				set_fnd(dealer.score, player.score);
 				if (dealer.score >= 17) {
-					dealer.state = STAND; 
-					printf("딜러는 STAND를 선택했습니다.");
+					dealer.state = STAND;
+					set_lcd("                Dealer     Stand");
+					
+				} else {
+					set_lcd("                Dealer       Hit");	
 				}
+				usleep(3 * 500000);
 				if (dealer.state == HIT) {
 					peek(&deck);
 					draw(&deck, &dealer_stack, &dealer);
 					printf(" => %d점", dealer.score);
-					write_fnd(fnd_dev, dealer.score, player.score);								// ######
+					set_lcd("                Dealer    Player");
+					set_fnd(dealer.score, player.score);
 				}
+				break;
 			}
-			// 턴 종료 
-		} 
-		printf("\n계속하시겠습니까? "); scanf("%d", &isContinue);
+		}
+		switch(score) {
+			case 1: 
+				printf("\n\nPlayer Win!");
+				set_lcd("                  Player Win!!  ");
+				set_dot(0);
+				break;
+			case -1:
+				printf("\n\nDealer Win!");
+				set_lcd("                  Dealer Win!!  ");
+				set_dot(1);
+				break;
+			case 0:
+				printf("\n\n무승부!");
+				set_lcd("                     Draw!!     ");
+				set_dot(2);
+				break;
+			default: error("burst error");
+		}
+		while(1) {
+			set_lcd("   Continue?        Press 5!    ");
+			int tt;
+			tt = get_tact();
+			if (tt == 5) {
+				isContinue = 1;
+			} else {
+				isContinue = 0;
+			}
+			break;
+		}
 		if (!isContinue) break;
 	}
 	return 0;
 }
-
 
 void error(char* message) {
 	fprintf(stderr, "%s\n", message);
@@ -204,6 +263,38 @@ void print_deck(Deck *D) {
 		printf(" %d ", number % 11 + 1);
 	}
 	printf(" ]");
+}
+
+void get_player_deck(Deck *D) {
+	int i;
+	for(i = 0; i < D->size; i++) {
+		char temp[1];
+		int num = D->list[i];
+		int number = num % 11 + 1;
+		strcat(player_arr, "-");
+		if (number == 11) {
+			strcat(player_arr, "J");
+		} else {
+			sprintf(temp, "%d", number);
+			strcat(player_arr, temp);
+		}
+	}
+}
+
+void get_dealer_deck(Deck *D) {
+	int i;
+	for(i = 0; i < D->size; i++) {
+		char temp[1];
+		int num = D->list[i];
+		int number = num % 11 + 1;
+		strcat(dealer_arr, "-");
+		if (number == 11) {
+			strcat(dealer_arr, "J");
+		} else {
+			sprintf(temp, "%d", number);
+			strcat(dealer_arr, temp);
+		}
+	}
 }
 
 int is_empty(Deck *D) {
@@ -267,7 +358,7 @@ unsigned char asc_to_fnd(int n) {
 	switch (n) {
 		case 0: c = ~0x3f; break;
 		case 1: c = ~0x06; break;
-		case 2: c = ~0x4b; break;
+		case 2: c = ~0x5b; break;
 		case 3: c = ~0x4f; break;
 		case 4: c = ~0x66; break;
 		case 5: c = ~0x6d; break;
@@ -280,7 +371,12 @@ unsigned char asc_to_fnd(int n) {
  	return c;
 }
 
-void write_fnd(int dev, int dealer_score, int player_score) {
+void set_fnd(int dealer_score, int player_score) {
+	fnd_dev = open("/dev/fnd", O_RDWR);
+	if (fnd_dev < 0) {
+		fprintf(stderr, "can't open FND (%d)", fnd_dev);
+		exit(2);
+	}
 	int fnd_int = dealer_score * 100 + player_score;
 	unsigned char fnd_char[4];
 	fnd_char[0] = asc_to_fnd(fnd_int / 1000);
@@ -289,11 +385,45 @@ void write_fnd(int dev, int dealer_score, int player_score) {
 	fnd_int = fnd_int % 100;
 	fnd_char[2] = asc_to_fnd(fnd_int / 10);
 	fnd_char[3] = asc_to_fnd(fnd_int % 10);
-	write(dev, fnd_char, sizeof(fnd_char));
+	write(fnd_dev, fnd_char, sizeof(fnd_char));
+	usleep(3 * 500000);
+	close(fnd_dev);
 }
 
-void write_lcd(int dev, char message*) {
-	write(dev, message, strlen(message)); // 또는 MAX_CLCD_CHAR
+void set_lcd(char P[]) {
+	lcd_dev = open("/dev/clcd", O_RDWR);
+	if (lcd_dev < 0) {
+		fprintf(stderr, "can't open LCD (%d)", lcd_dev);
+		exit(2);
+	}
+	write(lcd_dev, P, strlen(P)); // 또는 MAX_CLCD_CHAR
+	close(lcd_dev);
+}
+
+int get_tact() {
+	tact_dev = open("/dev/tactsw", O_RDWR);
+	if (tact_dev < 0) {
+		fprintf(stderr, "can't open TACT (%d)", tact_dev);
+		exit(2);
+	}
+	unsigned char c;
+	while(1) {
+		read(tact_dev, &c, sizeof(c));
+		if(c) break;
+	}
+	close(tact_dev);
+	return c;
+}
+
+void set_dot(int index) {
+	dot_dev = open("/dev/dot", O_RDWR);
+	if (dot_dev < 0) {
+		fprintf(stderr, "can't open DOT (%d)", dot_dev);
+		exit(2);
+	}
+	write(dot_dev, &pattern[index], sizeof(pattern[index]));
+  	usleep(3 * 500000);
+  	close(dot_dev);
 }
 
 //unsigned char get_tact(int tmo) {
